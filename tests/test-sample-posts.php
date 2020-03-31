8d704237488525ba91b665e968f26147415a6c9e
Caleb Stauffer <>
2020-03-31 10:27:09 -0400

    dump post IDs

--- tests/test-sample-posts.php
+++ tests/test-sample-posts.php
@@ -51,7 +51,7 @@ class Collection_Test_SamplePosts extends Collection_UnitTestCase {
 		add_filter( 'collection:' . $key . '/proper_items', function( $items ) { return array_map( 'get_post', $items ); } );
 
 		$runtime = $this->get_runtime( $key );
-
+var_dump( static::collection_callback() );
 		$this->assertEquals( count( static::collection_callback() ), count( $runtime ) );
 		$this->assertEquals(
 			static::collection_callback()[3],
